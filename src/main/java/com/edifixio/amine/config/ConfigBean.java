4ddf19d8f4b2ffc0d584fabff5006eb8cc342380
otchi <amineouchiha@hotmail.com>
2015-07-17 18:47:19 +0200

    loading config (not finished )

--- src/main/java/com/edifixio/amine/config/ConfigBean.java
+++ src/main/java/com/edifixio/amine/config/ConfigBean.java
@@ -1,11 +1,42 @@
 package com.edifixio.amine.config;
 
-public class ConfigBean {
-	
-	@SuppressWarnings("unused")
-	private String host;
-	@SuppressWarnings("unused")
-	private String[] indexes;
+import java.util.ArrayList;
+import java.util.Map;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class ConfigBean {
+	private String host;
+	private Map<String, ArrayList<String>> indexes;
+	
+	public ConfigBean(Document document) {
+		super();
+		Element root=document.getDocumentElement();
+		host=root.getAttribute("host");
+		
+	}
+	
+
+	
+	public String getHost() {
+		return host;
+	}
+	
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+
+
+	public Map<String, ArrayList<String>> getIndexes() {
+		return indexes;
+	}
+
+
+
+	public void setIndexes(Map<String, ArrayList<String>> indexes) {
+		this.indexes = indexes;
+	}
 
 }
