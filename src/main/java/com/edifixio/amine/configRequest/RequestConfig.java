1a6dc3f8930aaa737c16041b3c865040d47dc297
otchi <amineouchiha@hotmail.com>
2015-07-20 18:58:26 +0200

    f

--- src/main/java/com/edifixio/amine/configRequest/RequestConfig.java
+++ src/main/java/com/edifixio/amine/configRequest/RequestConfig.java
@@ -1,11 +1,12 @@
 package com.edifixio.amine.configRequest;
 
+import org.omg.CORBA.Request;
 import org.w3c.dom.Element;
 
 import com.edifixio.amine.config.ElasticConfig;
 
 @SuppressWarnings("serial")
-public class RequestConfig extends ElasticConfig<Search> {
+public class RequestConfig extends ElasticConfig<Request> {
 
 	public RequestConfig(Element element) throws ClassNotFoundException {
 		super(element);
